69ac932dbd0c3201f1becb6c9d76ff10da196058
OptiGE <optige@gmail.com>
2020-12-04 09:14:47 +0100

    Update rate works

--- salary_comparison.js
+++ salary_comparison.js
@@ -1,21 +1,41 @@
+//Remember to cache these
 var SEKtoUSD = 0
 var USDtoSEK = 0
 
-function updateCurrencyRates (fromCurrency, toCurrency) {
+
+function updateCurrencyRates (fromCurrency="USD", toCurrency="SEK") {
   const GETRequest = async () => {
     const response = await fetch(`https://api.exchangeratesapi.io/latest?base=${fromCurrency}&symbols=${toCurrency}`)
-    const json = await response.json() // extract JSON from the http response
-    
-    try{
-        SEKtoUSD = parseInt(json["rates"]["SEK"])
-    } catch(err){
-        console.log(`Failed to convert json currency rate to int: ${json["rates"]["SEK"]}`);
-        console.log(err)
-    }
-    USDtoSEK = 1/SEKtoUSD
+    raiseforstatus(response);
+    const currencyJson = await response.json()
+    updateCurrencies(currencyJson);
+    refreshScreen();
+  }
+  
+  GETRequest();
+}
+
+function updateCurrencies(currencyResponse){
+  try{
+      temp_SEKtoUSD = parseFloat(currencyResponse["rates"]["SEK"])
+      temp_USDtoSEK = 1.0/temp_SEKtoUSD
+
+      SEKtoUSD = temp_SEKtoUSD.toPrecision(5);
+      USDtoSEK = temp_USDtoSEK.toPrecision(5);
+
+  } catch(err){
+      console.log(err)
+      throw `Failed to convert json currency rate to int: ${currencyResponse["rates"]["SEK"]}`;
   }
 }
 
+
+function raiseforstatus(response){
+  console.log(response);
+  //throw not 200 OK
+}
+
 function refreshScreen(){
-    
+  document.getElementById("SEKRate").innerHTML = `🇸🇪 = ${USDtoSEK} USD`;
+  document.getElementById("USDRate").innerHTML = `🇺🇸 = ${SEKtoUSD} SEK`;
 }
